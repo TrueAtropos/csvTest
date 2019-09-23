f2825f9a45e1cb1c1e774edf0d6d66def38a7574
TrueAtropos <55702676+TrueAtropos@users.noreply.github.com>
2019-09-23 18:25:45 +0300

    Add files via upload

---
+++ csvTestHereAllWorks/DebugMod.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _ready():
+	TranslationServer.set_locale("ru")
+	
+func _physics_process(delta):
+	$Debug/HBox/VBox/L0.set_text(tr("KEYB_NG"))
+	$Debug/HBox/VBox/L1.set_text(tr("KEY194"))
+	$Debug/HBox/VBox/L2.set_text(tr("KEY200"))
+
